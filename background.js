0aad179c5165c3f30bee2ec818f242f73ef4233a
Vlad Velici <vlad.velici@gmail.com>
2013-04-13 19:24:34 +0300

    added notification thing on the icon

---
+++ background.js
@@ -0,0 +1,27 @@
+function update_icon() {
+    chrome.tabs.query({active: true}, function(tabs){
+    	var tab = tabs[0];
+        var url = tab.url;
+
+        api.getQaItems(url, function(data) {
+        	console.log("something", data.length);
+        	if (data.length > 0) {
+			    chrome.browserAction.setBadgeBackgroundColor({color:[190, 190, 190, 230]});
+			    chrome.browserAction.setBadgeText({text:data.length.toString()});
+			} else {
+				chrome.browserAction.setBadgeBackgroundColor({color:[190, 190, 190, 0]});
+				chrome.browserAction.setBadgeText({text:""});
+			}
+        });
+    });
+}
+
+//listen for new tab to be activated
+chrome.tabs.onActivated.addListener(function(activeInfo) {
+    update_icon();
+});
+
+//listen for current tab to be changed
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    update_icon();
+});
